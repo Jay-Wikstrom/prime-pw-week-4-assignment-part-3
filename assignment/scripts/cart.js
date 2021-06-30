80b9d92d29c61aca382485627fa6f999efd64c0f
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-06-30 10:32:58 -0500

    addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,17 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item){
+  basket.push(item);
+  return true;
+}
+
+//Test addItem function
+console.log(basket);
+addItem('Pants');
+addItem('Shoes');
+addItem('Jacket');
+console.log(basket);
